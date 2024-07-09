6f37bc786920b1687d7fc4e92fa94ee6ab8c222b
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-07-09 15:26:56 +0200

    Update HelloWorld.java

--- DAY 0/src/HelloWorld.java
+++ DAY 0/src/HelloWorld.java
@@ -7,10 +7,10 @@ public class HelloWorld
     public static void main(String[] args)
     {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
-        Scanner scan = new Scanner(System.in); // Created a Scanner object
-        String inputString = scan.nextLine(); // Read User Input
+        Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine();
 
-        System.out.println("Hello, World.\n" + inputString); // Output User Input
+        System.out.println("Hello, World.\n" + inputString);
 
 
     }
