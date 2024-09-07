1e15f1d995f37d0d0a617f68ee8919c9c3c77510
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-09-07 10:00:00 +0200

    Add Car.java files for DAY 3 and DAY 4

---
+++ DAY 3/src/Car.java
@@ -0,0 +1,115 @@
+public class Car
+{
+    int maxSpeed = 100;
+    int minSpeed = 0;
+
+    double weight = 4079;
+
+    boolean isTheCarOn = false;
+    char condition = 'A';
+    String nameOfCar = "Bentley";
+
+    double maxFuel = 16;
+    double currentFuel = 8;
+    double mpg = 26.4;
+
+    int numberOfPeopleInCar = 1;
+    int maxNumberOfPeopleInCar = 6;
+
+    public Car()
+    {
+
+    }
+
+    public Car(int customMaxSpeed, double customWeight, boolean customIsTheCarOn)
+    {
+        maxSpeed = customMaxSpeed;
+        weight = customWeight;
+        isTheCarOn = customIsTheCarOn;
+    }
+
+    public void printVariables()
+    {
+        System.out.println("This is the max speed: " + maxSpeed);
+        System.out.println("This is the min speed: " + minSpeed);
+        System.out.println("This is the Car weight: " + weight);
+        System.out.println("This is the Car state: " + isTheCarOn);
+        System.out.println("This is the Car condition: " + condition);
+        System.out.println("This is the Car name: " + nameOfCar);
+        System.out.println("Number of people in Car: " + numberOfPeopleInCar);
+    }
+
+    public void upgradeMinSpeed()
+    {
+        minSpeed = maxSpeed;
+        maxSpeed = maxSpeed + 1;
+    }
+
+    public void getIn()
+    {
+        if(numberOfPeopleInCar < maxNumberOfPeopleInCar)
+        {
+            numberOfPeopleInCar = numberOfPeopleInCar + 1; // or we can use: numberOfPeopleInCar++;
+            System.out.println("Someone got in the car");
+        }
+
+        else
+        {
+            System.out.println("The car is full. People in the car: " + numberOfPeopleInCar );
+            System.out.println("Maximum number of people in the car: " + maxNumberOfPeopleInCar);
+        }
+    }
+
+    public void getOut()
+    {
+        if(numberOfPeopleInCar > 0)
+        {
+            numberOfPeopleInCar--; // or we can use: numberOfPeopleInCar = numberOfPeopleInCar - 1;
+        }
+
+        else
+        {
+            System.out.println("No one is in the car, " + numberOfPeopleInCar + " people in the car.");
+        }
+    }
+
+    public  double howManyMilesTillOutOfGas()
+    {
+        return currentFuel * mpg;
+    }
+
+    public double maxMilesPerFillUp()
+    {
+        return maxFuel * maxSpeed;
+    }
+
+    public void turnTheCarOn()
+    {
+        if(!isTheCarOn)
+        {
+            isTheCarOn = true;
+        }
+
+        else
+        {
+            System.out.println("The car is already on, " + isTheCarOn);
+        }
+    }
+
+
+    public static void main(String args[])
+    {
+        Car tommyCar = new Car();
+        tommyCar.getOut();
+        tommyCar.getOut();
+        tommyCar.getIn();
+        tommyCar.getIn();
+        tommyCar.getIn();
+        tommyCar.getIn();
+        tommyCar.getIn();
+        tommyCar.getIn();
+        tommyCar.getIn();
+        tommyCar.turnTheCarOn();
+        tommyCar.turnTheCarOn();
+    }
+}
