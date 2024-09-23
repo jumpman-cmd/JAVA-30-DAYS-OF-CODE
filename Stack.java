80cb06ded79439546d3cdd40f75fbd59eabc4e3e
Yanga Masibulele Mdede <codeks7407@gmail.com>
2024-09-23 12:00:00 +0200

    Add Stack.java

---
+++ Stack.java
@@ -0,0 +1,56 @@
+public class Stack
+{
+    private int array[];
+    private int top;
+    private int capacity;
+
+    /*public boolean isEmpty()
+    {
+        return top == - 1;
+    }
+
+    public boolean isFull()
+    {
+        return top = capacity - 1;
+    }*/
+
+    Stack (int capacity)
+    {
+        this.array = new int[capacity];
+        this.capacity = capacity;
+        this.top = -1;
+    }
+
+    public void push(int item)
+    {
+        /*if (isFull())
+        {
+            throw new RuntimeException("Stack is full");
+        }*/
+
+
+        array[++top] = item;
+    }
+
+    public int pop()
+    {
+        /*if (isEmpty())
+        {
+            throw new RuntimeException("Stack is empty");
+        }*/
+
+
+        return array[top--];
+    }
+
+    public int peek()
+    {
+        /*if (isEmpty())
+        {
+            throw new RuntimeException("Stack is empty");
+        }*/
+
+
+        return array[top];
+    }
+}
